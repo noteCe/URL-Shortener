4c9ed49d3e8739a7a8cdd00a57b1f35070f6e593
DESKTOP-8R0Q6JT\Saliha <myselflearningzone@gmail.com>
2016-12-16 23:14:57 +0300

    Kullanıcı kayıt olma, oturum açma ve profilini güncelleme ile ilgili iskelet yapı oluşturuldu.

---
+++ LinkKS/LinkKS.Business/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkKS.Business
+{
+    interface IUser
+    {
+
+        Guid SignUp(string name, string email, string password);
+        Guid? SignIn(string email, string password);//şifre doğruysa userid, yanlışsa null
+        void UpdateProfile(Guid userId, string name, string email, string password);
+    }
+}
---
+++ LinkKS/LinkKS.Business/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkKS.Business
+{
+    public class User : IUser
+    {
+        public Guid? SignIn(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid SignUp(string name, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProfile(Guid userId, string name, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
