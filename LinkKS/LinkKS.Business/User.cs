ae7c164dbbe0181e8ecab3ffdd34b265099174e4
DESKTOP-8R0Q6JT\Saliha <myselflearningzone@gmail.com>
2016-12-17 00:05:51 +0300

    SignIn, SignUp,UpdateProfile metodları yazıldı.Test edildi.

--- LinkKS/LinkKS.Business/User.cs
+++ LinkKS/LinkKS.Business/User.cs
@@ -8,19 +8,60 @@ namespace LinkKS.Business
 {
     public class User : IUser
     {
+        #region SignIn
+
         public Guid? SignIn(string email, string password)
         {
-            throw new NotImplementedException();
+            using (var dc = new LinkKSDataContext())
+            {
+                var item = dc.USERs.FirstOrDefault(c => c.EMail == email && c.Password == password);
+
+                return item.ID;
+
+            }
         }
+
+        #endregion
+
+
+        #region SignUp
 
         public Guid SignUp(string name, string email, string password)
         {
-            throw new NotImplementedException();
+            using (var dc = new LinkKSDataContext())
+            {
+                var item = new USER();
+
+                item.ID = Guid.NewGuid();
+                item.Name = name;
+                item.EMail = email;
+                item.Password = password;
+                item.CreatedDate = DateTime.Now;
+
+                dc.USERs.InsertOnSubmit(item);
+                dc.SubmitChanges();
+
+                return item.ID;
+            }
         }
+
+        #endregion
+
+        #region UpdateProfile
 
         public void UpdateProfile(Guid userId, string name, string email, string password)
         {
-            throw new NotImplementedException();
+            using (var dc = new LinkKSDataContext())
+            {
+                var item = dc.USERs.First(c => c.ID == userId);
+                item.Name = name;
+                item.EMail = email;
+                item.Password = password;
+
+                dc.SubmitChanges();
+            }
         }
+        #endregion
+
     }
 }
--- LinkKS/LinkKS.Test/Program.cs
+++ LinkKS/LinkKS.Test/Program.cs
@@ -11,12 +11,20 @@ namespace LinkKS.Test
     {
         static void Main(string[] args)
         {
-            var link = new Link();
+            //var link = new Link();
             //var shortLink = link.Shorten("http://www.empatik.com", null, "12345");
             // link.UpdatePassword(new Guid("0D7FA621-1BBE-44C1-BBDF-D3B497FC16D5"), "789");
             // link.UpdateExpireDate(new Guid("0D7FA621-1BBE-44C1-BBDF-D3B497FC16D5"), new DateTime(2016, 12, 16));
             //link.UpdateNotification(new Guid("0D7FA621-1BBE-44C1-BBDF-D3B497FC16D5"), true);
-            link.UpdateOneShot(new Guid("0D7FA621-1BBE-44C1-BBDF-D3B497FC16D5"), true);
+            //link.UpdateOneShot(new Guid("0D7FA621-1BBE-44C1-BBDF-D3B497FC16D5"), true);
+
+            var user = new User();
+            // var id = user.SignUp("saliha", "salihayesilyurt@hotmail.com", "1234");
+            //user.UpdateProfile(new Guid("EE05A40E-04E6-4E36-998F-B93A6DB62568"), "saliha", "salihayesilyurt@hotmail.com", "753");
+
+            var item = user.SignIn("salihayesilyurt@hotmail.com", "753");
+            Console.WriteLine(item);
+            Console.ReadKey();
 
         }
     }
