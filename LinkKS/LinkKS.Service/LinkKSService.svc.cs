08edcd1ab51ef4c3716068982b350616fd22a7d1
DESKTOP-8R0Q6JT\Saliha <myselflearningzone@gmail.com>
2016-12-22 23:41:09 +0300

    WCF Service App oluşturuldu, (LinkKSService) -- LinkKSServiceContract implemente edildi. Business library'e bağlanarak metodları kullanıldı. --Add Reference diyerek, solution'daki LinkKS.Business ve LinkKS.ServiceContract projeleri referans olarak eklendi.---

---
+++ LinkKS/LinkKS.Service/LinkKSService.svc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using LinkKS.ServiceContract;
+using LinkKS.Business;
+
+namespace LinkKS.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "LinkKSService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select LinkKSService.svc or LinkKSService.svc.cs at the Solution Explorer and start debugging.
+    public class LinkKSService : ILinkKSServiceContract
+    {
+        public void UpdateLinkPassword(Guid linkId, string password)
+        {
+            var link = new Link();
+            link.UpdatePassword(linkId, password);
+        }
+
+        public Guid UserSignUp(string name, string eMail, string password)
+        {
+            var user = new User();
+           return user.SignUp(name, eMail, password);
+        }
+    }
+}
--- LinkKS/LinkKS.ServiceContract/ILinkKSServiceContract.cs
+++ LinkKS/LinkKS.ServiceContract/ILinkKSServiceContract.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace LinkKS.ServiceContract
 {
     [ServiceContract]
-    interface ILinkKSServiceContract
+    public interface ILinkKSServiceContract
     {
 
         [OperationContract]
